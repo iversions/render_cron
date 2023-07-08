b308affc1a975599ccfef9ad94ce85eaa58653a5
Shashikant Vishwakarma <57395523+iversions@users.noreply.github.com>
2023-07-08 18:38:39 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -54,11 +54,11 @@ def checkdate(string):
 
 
 def invextract(path):
-    http = urllib3.PoolManager()
-    temp = io.BytesIO()
-    temp.write(http.request("GET",path).data)
+    # http = urllib3.PoolManager()
+    # temp = io.BytesIO()
+    # temp.write(http.request("GET",path).data)
     el = []
-    with pdfplumber.open(temp) as page:
+    with pdfplumber.open(path) as page:
         firstpage = page.pages[0]
         charst = firstpage.extract_words()
         f_ext_text = firstpage.extract_text()
